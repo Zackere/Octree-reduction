4e47f0b5458f622b522c3f3bdd2339b64efc7dfc
Zackere <wo1001@wp.pl>
2019-11-24 11:19:19 +0100

    Implement node reduction

--- Octree/include/octree.hpp
+++ Octree/include/octree.hpp
@@ -2,19 +2,25 @@
 
 #include <cstdint>
 #include <memory>
+#include <set>
 
 namespace gk3 {
 class Octree {
  public:
   Octree();
   void InsertColor(uint32_t color);
+  void Reduce(unsigned max_colors);
 
  private:
   struct OctreeNode {
-    uint64_t refs = 0;
+    uint64_t refs = 0, level = 0;
     uint64_t r = 0, g = 0, b = 0;
     std::unique_ptr<OctreeNode> children[8] = {nullptr};
+    int Reduce();
   };
   std::unique_ptr<OctreeNode> root_ = nullptr;
+  std::set<OctreeNode*> nodes_on_level_[8];
+  uint8_t last_nonempty_set_ = 0;
+  uint64_t number_of_leaves_ = 0;
 };
 }  // namespace gk3
--- Octree/src/octree.cpp
+++ Octree/src/octree.cpp
@@ -4,22 +4,66 @@ namespace gk3 {
 namespace {
 extern "C" uint8_t __cdecl GetIndex(uint8_t iteration, uint32_t color);
 }  // namespace
-Octree::Octree() : root_(new OctreeNode) {}
+Octree::Octree() : root_(new OctreeNode) {
+  nodes_on_level_[0].insert(root_.get());
+}
 
 void Octree::InsertColor(uint32_t color /*in ARGB format*/) {
   uint8_t index = 0;
   auto* cur_node = root_.get();
-  for (int8_t i = 7; i >= 0; --i) {
-    index = GetIndex(i, color);
-    if (!cur_node->children[index])
-      cur_node->children[index] = std::make_unique<OctreeNode>();
+  for (uint8_t i = 1; i < 8; ++i) {
+    index = GetIndex(8 - i, color);
+    if (!cur_node->children[index]) {
+      nodes_on_level_[i].insert(
+          (cur_node->children[index] = std::make_unique<OctreeNode>()).get());
+      cur_node->children[index]->level = i;
+    }
     cur_node = cur_node->children[index].get();
   }
+  last_nonempty_set_ = 7;
+  index = GetIndex(0, color);
+  if (!cur_node->children[index]) {
+    cur_node->children[index] = std::make_unique<OctreeNode>();
+    cur_node->children[index]->level = 8;
+    ++number_of_leaves_;
+  }
+  cur_node = cur_node->children[index].get();
+
   ++cur_node->refs;
   cur_node->b += color & 0xFF;
   color >>= 8;
   cur_node->g += color & 0xFF;
   color >>= 8;
   cur_node->r += color & 0xFF;
+}
+
+void Octree::Reduce(unsigned max_colors) {
+  if (max_colors == 0)
+    return;
+  while (number_of_leaves_ > max_colors) {
+    auto* min_refs = *nodes_on_level_[last_nonempty_set_].begin();
+    for (auto* node : nodes_on_level_[last_nonempty_set_])
+      if (min_refs->refs > node->refs)
+        min_refs = node;
+    number_of_leaves_ -= min_refs->Reduce();
+    nodes_on_level_[last_nonempty_set_].erase(min_refs);
+    if (nodes_on_level_[last_nonempty_set_].empty())
+      --last_nonempty_set_;
+  }
+}
+
+int Octree::OctreeNode::Reduce() {
+  int ret = 0;
+  for (auto& child : children) {
+    if (child.get()) {
+      refs += child->refs;
+      r += child->r;
+      g += child->g;
+      b += child->b;
+      child.reset();
+      ++ret;
+    }
+  }
+  return ret - 1;
 }
 }  // namespace gk3
